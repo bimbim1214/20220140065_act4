2a90fabcb1e2aed5d9949180c21c8f207f7bdcaa
bimbim1214 <bimoadityapangestu@gmail.com>
2025-04-23 16:08:54 +0700

    Membuat class OrderPage sebagai StatefulWidget untuk halaman pemesanan

---
+++ lib/order_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan4/detail_order_page.dart';
+//import 'package:play_navigation/presentation/detail_order_page.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
