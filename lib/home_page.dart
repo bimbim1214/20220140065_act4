37f10eaa446bf44bcf34c4c31a9c1712a7c95fa2
bimbim1214 <bimoadityapangestu@gmail.com>
2025-04-23 16:03:31 +0700

    Membuat class HomePage sebagai StatefulWidget untuk halaman utama

---
+++ lib/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan4/order_page.dart';
+//import 'package:play_navigation/presentation/order_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
