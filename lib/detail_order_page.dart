447420c62cbc616b0b01f914e80dce53fa9ad657
bimbim1214 <bimoadityapangestu@gmail.com>
2025-04-23 16:29:58 +0700

    Membuat class DetailOrderPage untuk menampilkan detail pesanan

---
+++ lib/detail_order_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan4/home_page.dart';
+//import 'package:play_navigation/presentation/home_page.dart';
+
+class DetailOrderPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+  const DetailOrderPage({
+    super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga
+  });
+
+  
