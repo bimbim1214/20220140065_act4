7ffb7890d4786ec929cf6e161151fd7a03eca097
bimbim1214 <bimoadityapangestu@gmail.com>
2025-04-23 15:51:31 +0700

    Membuat class RegisterPage sebagai StatefulWidget untuk halaman pendaftaran

---
+++ lib/register_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
